5270c54036d4ef8d4d2f26cffa3ffc0bb3c1d9f0
Eric Wang (DEVDIV) <ericwan@microsoft.com>
2020-08-24 12:22:51 +0800

    Apply to Azure Spring Cloud

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "Greetings from Spring Boot! Time now is " + java.time.Clock.systemUTC().instant();
 	}
 
 }
